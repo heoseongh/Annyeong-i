fe4b7395b017941120b3459977a1e728ce8fa612
heoseongh <shheo0807@gmail.com>
2021-05-27 08:19:23 +0900

    ADD: text to speech

---
+++ api/gcloudSpeechAPI.py
@@ -0,0 +1,32 @@
+from playsound import playsound
+
+def toSpeech(text):
+    """Synthesizes speech from the input string of text."""
+    from google.cloud import texttospeech
+
+    client = texttospeech.TextToSpeechClient()
+
+    input_text = texttospeech.SynthesisInput(text=text)
+
+    # 다음 공식 문서에서 목소리 선택 가능
+    # https://cloud.google.com/text-to-speech/docs/voices
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="ko-KR",
+        name="ko-KR-Wavenet-A",
+        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    response = client.synthesize_speech(
+        request={"input": input_text, "voice": voice, "audio_config": audio_config}
+    )
+
+    # The response's audio_content is binary.
+    with open("answer.mp3", "wb") as out:
+        out.write(response.audio_content)
+        print('mp3 파일이 성공적으로 생성되었습니다.')
+        
+    playsound('answer.mp3')
